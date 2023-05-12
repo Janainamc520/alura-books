c4c94026740b2fbc05d892e487c7bbd3c5691404
Janaina Moreira Clemente <janainamc520@gmail.com>
2023-05-12 18:32:03 -0300

    ok

--- src/componentes/Logo/index.js
+++ src/componentes/Logo/index.js
@@ -1,15 +1,25 @@
-import './estilo.css'
 import logo from '../../imagens/logo.svg'
+import styled from 'styled-components'
+
+const LogoContainer = styled.div`
+    display: flex;
+    font-size: 30px;
+`
+
+const LogoImage = styled.img`
+    margin-right: 10px;
+`
+
 function Logo() {
     return (
-        <div className='logo'>
-            <img
+        <LogoContainer>
+            <LogoImage
                 src={logo}
-                alt='logo'
-                className='logo-img'
-            ></img>
-            <p><strong>Universo</strong>Books</p>
-        </div>
+                alt='logo' 
+            />
+            <p><strong>Alura</strong>Books</p>
+       </LogoContainer>
     )
 }
+
 export default Logo
